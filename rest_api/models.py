b3fe3b77552807a1b4466e9c239bb297ef675583
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-10 14:19:05 +0600

    added published date in book model

--- rest_api/models.py
+++ rest_api/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    Published_Date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
