1b5f7d6343b5ef0d274f600621f924401798569f
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-10 13:31:06 +0600

    Book get, post

---
+++ rest_api/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_api.models import Book
+from rest_api.serializers import BookSerializer
+from rest_framework import status
+# Create your views here.
+
+class BookView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BookSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
